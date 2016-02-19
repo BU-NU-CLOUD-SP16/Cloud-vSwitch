81f6689be9711b54d5c42ac1580046a6946f3d98
jas911 <jas91@cs.bu.edu>
2016-02-19 17:19:57 +0000

    Sails auth

--- vSwitch-API/api/models/User.js
+++ vSwitch-API/api/models/User.js
@@ -4,6 +4,7 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+var bcrypt = require('bcrypt');
 
 module.exports = {
 
@@ -16,14 +17,29 @@ module.exports = {
       type: 'string',
       required: true
     },
-    email: {
-      type: 'string',
-      required: true
-    },
     //password,
     organizations: {
       collection: 'organization',
       via: 'members'
+    },
+    email: {
+  		type: 'email',
+  		required: true,
+  		unique: true
+  	},
+
+  	password: {
+  		type: 'string',
+      required: true,
+  	},
+
+    active: {
+      type: 'boolean',
+      defaultsTo: true
+    },
+
+    isPasswordValid: function (password, cb) {
+      bcrypt.compare(password, this.password, cb);
     }
   }
 };
