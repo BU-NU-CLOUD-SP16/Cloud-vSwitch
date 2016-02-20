1f03adaa58d4680af285d5d0b5069025b1e287ab
jas911 <jas91@cs.bu.edu>
2016-02-20 15:55:12 +0000

    Organization and Instance form validation

--- vSwitch-UI/app/scripts/controllers/instances.js
+++ vSwitch-UI/app/scripts/controllers/instances.js
@@ -8,62 +8,57 @@
  * Controller of the vSwitchUiApp
  */
 angular.module('vSwitchUiApp')
-  .controller('InstanceCtrl', function ($scope) {
+  .controller('InstanceCtrl', function($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    
+
     // Local storage
     $scope.organizations = JSON.parse(localStorage.getItem('organizations'));
     var current = localStorage.getItem("current");
-    
+
     // Scope variables
     $scope.organization = $scope.organizations[current]
     $scope.instances = $scope.organization.instances;
     $scope.code = "";
-    
+
     // Scope functions
     $scope.add_instance = add_instance;
     $scope.rem_instance = rem_instance;
     $scope.edit_instance = edit_instance;
     $scope.show_code = show_code;
-    
-    
+
+
     // Functions
-    
+
     function add_instance() {
-      if (typeof($scope.instance_name) == "undefined" || $scope.instance_name == "") {
-        alert("Must type a name for the instance");
-        return;
-      } else {
-        
-        //TODO: call service add_organization
-        $scope.instances.push($scope.instance_name);
-        localStorage.setItem('organizations', JSON.stringify($scope.organizations));
-        $scope.instance_name = "" 
-      }
+      //TODO: call service add_organization
+      $scope.instances.push($scope.instance_name);
+      localStorage.setItem('organizations', JSON.stringify($scope.organizations));
+      $scope.instance_name = ""
 
     }
-    
+
     function rem_instance(index) {
       if (confirm("Are yoy sure you want to delete instance " + $scope.instances[index]))
         $scope.instances.splice(index, 1);
-        localStorage.setItem('organizations', JSON.stringify($scope.organizations));
+      localStorage.setItem('organizations', JSON.stringify($scope.organizations));
     }
-    
+
     function edit_instance(index) {
 
       localStorage.setItem('organizations', JSON.stringify($scope.organizations));
-      
+
       //TODO: call service edit_organization  
     }
-    
+
     function show_code() {
       if ($scope.code == "") {
         $scope.code = $scope.organization.code;
-      } else {
+      }
+      else {
         $scope.code = "";
       }
     }
--- vSwitch-UI/app/scripts/controllers/organizations.js
+++ vSwitch-UI/app/scripts/controllers/organizations.js
@@ -8,80 +8,81 @@
  * Controller of the vSwitchUiApp
  */
 angular.module('vSwitchUiApp')
-  .controller('OrgCtrl', function ($scope, $location) {
+  .controller('OrgCtrl', function($scope, $location) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    
+
     // Local Storage
-    $scope.organizations = localStorage.getItem('organizations') == null ? []: JSON.parse(localStorage.getItem('organizations'));
-    
+    $scope.organizations = localStorage.getItem('organizations') == null ? [] : JSON.parse(localStorage.getItem('organizations'));
+
     // Scope variables
-    $scope.organization  = {};
-    
+    $scope.organization = {};
+
     // Scope functions
-    $scope.add_organization  = add_organization;
+    $scope.add_organization = add_organization;
     $scope.join_organization = join_organization;
-    $scope.rem_organization  = rem_organization;
+    $scope.rem_organization = rem_organization;
     $scope.edit_organization = edit_organization;
-    $scope.view_instances    = view_instances;
-    $scope.installer         = installer;
-    
-    
+    $scope.view_instances = view_instances;
+    $scope.installer = installer;
+
+
     // Functions
-    
+
     function add_organization() {
-      if (typeof($scope.organization.name) == "undefined" || $scope.organization.name == "") {
-        alert("Must type a name for the organization");
-        return;
-      } else {
-        
-        //TODO: call service add_organization
-  
-        $scope.organizations.push({name:$scope.organization.name, instances:[], code: "MYCODE"});
-        localStorage.setItem('organizations', JSON.stringify($scope.organizations));
-        $scope.organization.name = "" 
-      }
+
+      //TODO: call service add_organization
+
+      $scope.organizations.push({
+        name: $scope.organization.name,
+        instances: [],
+        code: "MYCODE"
+      });
+      localStorage.setItem('organizations', JSON.stringify($scope.organizations));
+      $scope.organization.name = ""
+
     }
-    
+
     function join_organization() {
-      if (typeof($scope.organization.code) == "undefined" || $scope.organization.code == "") {
-        alert("Must type an Invitation code");
-        return;
-      } else {
-        var code = $scope.organization.code;
-        
-        //TODO: Call service join_organization(code) 
-        $scope.organizations.push({name:"Organization", instances:[], code: "MYCODE"});
-        localStorage.setItem('organizations', JSON.stringify($scope.organizations));
-        $scope.organization.code = "" 
-      }
+
+      var code = $scope.organization.code;
+
+      //TODO: Call service join_organization(code) 
+      $scope.organizations.push({
+        name: "Organization",
+        instances: [],
+        code: "MYCODE"
+      });
+      localStorage.setItem('organizations', JSON.stringify($scope.organizations));
+      $scope.organization.code = ""
+
     }
-    
+
     function edit_organization(index) {
 
       localStorage.setItem('organizations', JSON.stringify($scope.organizations));
-      
+
       //TODO: call service edit_organization  
     }
-    
+
     function rem_organization(index) {
-      if (confirm("Are you sure you want to remove " + $scope.organizations[index])) 
+      if (confirm("Are you sure you want to remove " + $scope.organizations[index]))
         $scope.organizations.splice(index, 1);
-        
+
       localStorage.setItem('organizations', JSON.stringify($scope.organizations));
     }
-    
+
     function view_instances(index) {
       localStorage.setItem('current', index);
-      $location.path('/instances'); 
+      $location.path('/instances');
     }
-    
+
     function installer() {
       alert("Downloading installer")
-      
+
     }
-    
+
   });
