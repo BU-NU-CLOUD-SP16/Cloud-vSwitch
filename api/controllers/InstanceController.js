e5ab84f9fee2022a5c7b2b62419bc25ccca1a9b8
Javier Arguello <jarguellososa@gmail.com>
2016-03-05 19:30:28 -0500

    Integrated API with MOC

--- api/controllers/InstanceController.js
+++ api/controllers/InstanceController.js
@@ -5,7 +5,82 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var moc = require('../services/MOC.js');
+var request = require('request');
+
 module.exports = {
-	
+    create: 
+        function(req, res) {
+            instance = req.body
+            // MOC create instance
+            moc.create(instance, function(err, instance) {
+                if (err) {
+                    return res.status(400).json()
+                }
+                Instance.create(instance).exec(function(err,instance) {
+                    if (err) {
+                        return res.json(err.status, {
+                            err: err
+                        });
+                    }
+                    return res.status(200).json(instance)
+                })
+            })
+
+        },
+
+    start: 
+        function(req, res) {    
+            instance = req.body
+            console.log(instance)
+            moc.start(instance, function(err,instance) {
+                console.log(err)
+                if (err) {
+                    return res.status(400).json()
+                }
+                return res.status(200).json(instance)
+            })
+        },
+    stop: 
+        function(req, res) {
+            instance = req.body
+            moc.stop(instance, function(err, instance) {
+                if (err) {
+                    return res.status(400).json()
+                }
+                return res.status(200).json(instance)
+            })
+        },
+
+    destroy:
+        function(req, res) {
+            instance = req.params.id
+            Instance.findOne({
+                id: req.params.id
+            }).exec(function(err, instance) {
+                if (err) {
+                    return res.status(400).json()
+                }
+                if (!instance) {
+                    return res.status(404).json()
+                }
+            
+                moc.terminate(instance, function(err,instance) {
+                    if (err) {
+                        return res.status(400).json()
+                    }
+                    Instance.destroy(instance).exec(function(err,instance) {
+                        if (err) {
+                            return res.status(400).json({
+                                err: {error: err}
+                            });
+                        } else {
+                            return res.status(200).json(instance)
+                        }
+                    })
+                })
+            })
+        }
 };
+
 
--- api/controllers/OrganizationController.js
+++ api/controllers/OrganizationController.js
@@ -6,6 +6,23 @@
  */
 
 module.exports = {
-	
+    create: 
+        function(req, res) {
+            organization = req.body
+            console.log(organization)
+            organization.code = organization.name + "-" + organization.owner.substr(organization.owner.length - 5) + Math.floor((Math.random() * 100) + 1);
+            Organization.create(organization).exec(function(err,organization) {
+                if (err) {
+                    if (err.ValidationError) {
+                        return helper.convertWaterlineError(err, res);
+                    }
+                    return res.json(err.status, {
+                        err: err
+                    });
+                }
+                return res.status(200).json(organization)
+            })
+    
+        }
 };
 
