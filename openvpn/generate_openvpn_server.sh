0c5fbeea3d193d3ea4048bd500dfe8595c84cc5a
shwsun <shwsun@bu.edu>
2016-03-21 17:23:38 -0400

    omg what have I done?

---
+++ openvpn/generate_openvpn_server.sh
@@ -0,0 +1,56 @@
+## To run
+# bash generate_openvpn_server.sh
+
+echo "
+
+#!/bin/bash
+
+#Update Ubuntus repository lists.
+apt-get update
+
+#Install OpenVPN and Easy-RSA.
+apt-get -y install openvpn easy-rsa
+
+#OpenVPN configuration file
+cat > /etc/openvpn/server.conf << EOL
+#OpenVPN configuration file
+port 80
+proto tcp
+dev tun
+server 10.8.0.0 255.255.255.0
+ifconfig-poolem-persist ipp.txt
+push \"redirect-gateway def1 bypass-dhcp\"
+push \"dhcp-option DNS 208.67.222.222\"
+push \"dhcp-option DNS 208.67.220.220\"
+duplicate-cn
+keepalive 10 120
+comp-lzo
+user nobody
+group nogroup
+persist-key
+persist-tun
+verb 3
+dh dh2048.pem
+ca ca.crt
+cert server.crt
+key server.key
+EOL
+
+chmod 755 /etc/openvpn/server.conf
+
+
+cat > /etc/openvpn/ca.crt << EOL
+`cat $1`
+EOL
+
+cat > /etc/openvpn/server.crt << EOL
+`cat $2`
+EOL
+
+cat > /etc/openvpn/server.key << EOL
+`cat $3`
+EOL
+
+
+
+" >> openvpn_server.sh
