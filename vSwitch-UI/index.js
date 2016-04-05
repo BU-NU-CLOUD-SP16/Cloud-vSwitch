adeca3e7b64ae78261d877c5189cdc5ec7f0ace0
Javier Arguello <jarguellososa@gmail.com>
2016-04-04 21:11:03 -0400

    Clodu vSwitch
        vSwitch UI
            Pushing to Heroku

---
+++ vSwitch-UI/index.js
@@ -0,0 +1,14 @@
+// server.js
+
+// set up ========================
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+
+// configuration =================
+
+app.use(express.static(__dirname + '/app'));                 // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname));                 // set the static files location /public/img will be /img for users
+
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
