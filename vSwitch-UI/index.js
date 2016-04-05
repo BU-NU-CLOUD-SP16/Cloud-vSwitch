23b485feff62a7b8e0246f9ee25c65e4e26c64e1
Javier Arguello <jarguellososa@gmail.com>
2016-04-04 21:32:43 -0400

    Cloud vSwitch
        vSwitch UI
            Changing cors policy

--- vSwitch-UI/index.js
+++ vSwitch-UI/index.js
@@ -10,9 +10,15 @@ var port = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/app'));                 // set the static files location /public/img will be /img for users
 app.use(express.static(__dirname));                 // set the static files location /public/img will be /img for users
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // listen (start app with node server.js) ======================================
-var server = http.createServer(app)
+var server = http.createServer(app);
 server.listen(port);
+var io = require('socket.io')(server, {origins:'*'});
+
 console.log("http server listening on %d", port)
 
