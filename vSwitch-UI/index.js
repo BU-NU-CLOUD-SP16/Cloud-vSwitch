d55dcbbecf4d8a160f2f04181a6dddcafd8b817f
Javier Arguello <jarguellososa@gmail.com>
2016-04-04 21:30:07 -0400

    Cloud vSwitch
        vSwitch UI
            Pushing to heroku
            index.js

--- vSwitch-UI/index.js
+++ vSwitch-UI/index.js
@@ -3,6 +3,8 @@
 // set up ========================
 var express  = require('express');
 var app      = express();                               // create our app w/ express
+var http = require("http")
+var port = process.env.PORT || 5000
 
 // configuration =================
 
@@ -10,5 +12,7 @@ app.use(express.static(__dirname + '/app'));                 // set the static f
 app.use(express.static(__dirname));                 // set the static files location /public/img will be /img for users
 
 // listen (start app with node server.js) ======================================
-app.listen(80);
-console.log("App listening on port 8080");
+var server = http.createServer(app)
+server.listen(port);
+console.log("http server listening on %d", port)
+
